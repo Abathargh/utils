b8bb2318179bc67343eaf164545e02d0c0103a3e
mar <gim.marcello@gmail.com>
2021-07-11 11:31:06 +0200

    fixed: deleted unescaped newline character

--- lubuntu-setup/setup.sh
+++ lubuntu-setup/setup.sh
@@ -4,4 +4,4 @@ theme_path="/usr/share/themes/Lubuntu Arc/openbox-3"
 sudo mv "${theme_path}/themerc" "${theme_path}/themerc_bkp"
 sudo cp themerc_lubuntu  "${theme_path}/themerc"
 
-echo "Logout and login again to apply the changes\n"
+echo "Logout and login again to apply the changes"
